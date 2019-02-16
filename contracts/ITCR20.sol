ba4b5c1ad743fe76fa8378ae61f992766b9b1788
Marto <martinlsanchez@gmail.com>
2019-02-16 04:45:30 -0300

    Added ITCR20 interface to be implemented in Simple TCR

---
+++ contracts/ITCR20.sol
@@ -0,0 +1,41 @@
+pragma solidity ^0.4.24;
+
+import "./zeppelin/IERC20.sol";
+
+interface ITCR20 {
+
+    function name() public returns(string);
+    function description() public returns(string);
+
+    function acceptedDataTypes() public returns(string);
+    function voteScheme() public returns(string);
+    function tokenScheme() public returns(string);
+    function exitScheme() public returns(string);
+
+    function token() public returns(IERC20);
+
+    // Main functions
+    function submit(bytes32 _listingHash, uint _tokenAmount, string _data) external;
+    function getListingData(bytes32 _listingHash) external returns (string memory jsonData);
+    function challenge(bytes32 _listingHash, uint _tokenAmount, string _data) external returns (uint challengeID);
+    function vote(uint _challengeID, uint _tokenAmount, uint[] _data) external;
+    function claimChallengeReward(uint _challengeID) public;
+    function claimVoterReward(uint _challengeID) public;
+    function exit(bytes32 _listingHash, string _data) external;
+
+    // Helpers/optional functions
+    function isWhitelisted(bytes32 _listingHash) public view returns (bool whitelisted);
+    function challengeExists(bytes32 _listingHash) public view returns (uint lastChallengeID);
+    function challengeCanBeResolved(bytes32 _listingHash) public view returns (bool need);
+    function voterReward(address _voter, uint _challengeID) public view returns (uint tokenAmount);
+    function challengeReward(address _applierOrChallenger, uint _challengeID) public view returns (uint tokenAmount);
+
+    event _Application(bytes32 indexed listingHash, uint deposit, uint appEndDate, string data, address indexed applicant);
+    event _Challenge(bytes32 indexed listingHash, uint challengeID, string data, uint commitEndDate, uint revealEndDate,
+     address indexed challenger);
+    event _Vote(uint indexed challengeID, uint numTokens, address indexed voter);
+    event _ChallengeResolved(bytes32 indexed listingHash, uint indexed challengeID, uint rewardPool, uint totalTokens, bool success);
+    event _ApplicationWhitelisted(bytes32 indexed listingHash);
+    event _ListingExited(bytes32 indexed listingHash);
+}
+

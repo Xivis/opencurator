61cd1408bc5639feebf17f42a2a408fba6365049
Marto <martinlsanchez@gmail.com>
2019-02-16 16:27:54 -0300

    Fix in ITCR20

--- contracts/ITCR20.sol
+++ contracts/ITCR20.sol
@@ -6,13 +6,11 @@ interface ITCR20 {
 
     function name() public view returns(string);
     function description() public view returns(string);
-
     function acceptedDataType() public view returns(string);
     function applyScheme() public view returns(string);
     function voteScheme() public view returns(string);
     function exitScheme() public view returns(string);
     function tokenScheme() public view returns(string);
-
     function token() public view returns(IERC20);
 
     // Main functions
@@ -25,17 +23,17 @@ interface ITCR20 {
     function exit(bytes32 _listingHash, string _data) external;
 
     // Getters and Helpers functions
-    function getParameter(string pName) returns (uint pValue);
-
+    function getParameter(string pName) public view returns(uint pValue);
     function isWhitelisted(bytes32 _listingHash) public view returns (bool whitelisted);
     function challengeExists(bytes32 _listingHash) public view returns (uint lastChallengeID);
     function challengeCanBeResolved(bytes32 _listingHash) public view returns (bool need);
     function voterReward(address _voter, uint _challengeID) public view returns (uint tokenAmount);
     function challengeReward(address _applierOrChallenger, uint _challengeID) public view returns (uint tokenAmount);
 
+    // Events
     event _Application(bytes32 indexed listingHash, uint deposit, uint appEndDate, address indexed applier, string data);
     event _Challenge(bytes32 indexed listingHash, uint challengeID, uint voteEndDate, address indexed challenger, string data);
-    event _Vote(uint indexed challengeID, uint numTokens, address indexed voter, uint[] _data);
+    event _Vote(uint indexed challengeID, uint numTokens, address indexed voter, string _data);
     event _ChallengeResolved(bytes32 indexed listingHash, uint indexed challengeID, uint rewardPool, uint totalTokens, bool success);
     event _ApplicationWhitelisted(bytes32 indexed listingHash);
     event _ListingExited(bytes32 indexed listingHash, uint voteEndDate, string data);
